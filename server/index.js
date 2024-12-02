966505ed4c5be8092833aaed8ed9f6e57a537a08
michaelWong <michael.wong@pixent.net>
2024-12-03 07:43:47 +0800

    revert

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api", keywordRoutes);
+app.use("/api", keywordRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
