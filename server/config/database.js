a650875c0ca7defd909f72e8c56621fa30dee46d
michaelWong <michael.wong@pixent.net>
2024-12-03 09:05:42 +0800

    fix api

--- server/config/database.js
+++ server/config/database.js
@@ -10,11 +10,11 @@ const pool = new Pool({
     // database: process.env.DB_NAME,
     // password: process.env.DB_PASSWORD,
     // port: process.env.DB_PORT,
-    // user: "keyword_search_user",
-    // host: "dpg-ct6d683qf0us738ecu60-a",
-    // database: "keyword_search",
-    // password: "V0DLXa5nKqWEsDMePXflgaR68UxPlFn8",
-    // port: 5432,
+    user: "keyword_search_user",
+    host: "dpg-ct6d683qf0us738ecu60-a",
+    database: "keyword_search",
+    password: "V0DLXa5nKqWEsDMePXflgaR68UxPlFn8",
+    port: 5432,
     connectionString: "postgresql://keyword_search_user:V0DLXa5nKqWEsDMePXflgaR68UxPlFn8@dpg-ct6d683qf0us738ecu60-a.singapore-postgres.render.com/keyword_search",
     ssl: {
         rejectUnauthorized: false // Important for Render's SSL requirement
